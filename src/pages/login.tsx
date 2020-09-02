1f37663d6062c4dfe889f69ec29a128047118028
kterui9019 <pleasure.teru@gmail.com>
2020-09-02 22:45:30 +0900

    #1 とりあえずFirebaseとの認証ができるようにした

---
+++ src/pages/login.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import FirebaseAuth from '../components/FirebaseAuth'
+
+const LoginPage = () => (
+  <Layout title="Login | Next.js + TypeScript Example">
+    <h1>Login 🗝</h1>
+    <div>
+      <FirebaseAuth />
+    </div>
+    <p>
+      <Link href="/">
+        <a>home</a>
+      </Link>
+    </p>
+  </Layout>
+)
+
+export default LoginPage
