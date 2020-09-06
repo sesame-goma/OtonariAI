5cdf8fd534d7277956d66df8d0f730da5886ccba
sesame-goma <wataru.tatsuda@gmail.com>
2020-09-06 18:42:46 +0900

    d4 不要な構成を削除し、package.jsonを更新

--- src/utils/hooks/useFormInput.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export default function useFormInput (initialValue: string){
-  const [value, setValue] = useState(initialValue);
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
-  return {value, onChange: handleChange };
-};
