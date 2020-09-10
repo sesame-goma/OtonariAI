8573570fd8a82f87a7720ffbc98c139eab1f71d7
sesame-goma <wataru.tatsuda@gmail.com>
2020-09-10 18:24:59 +0900

    d5 グラフ表示まで

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -6,6 +6,7 @@ import {
   Data,
   User,
  } from '../../types';
+ import { Country } from '../../types/analytics';
 import {
   sampleAnalyticData,
   sampleUserData
@@ -14,14 +15,20 @@ import Layout from '../../components/Layout';
 import List from '../../components/List';
 
 type Props = {
-  data: Data,
+  dataCountries: any,
   items: User[]
 }
 
-const WithStaticProps = ({ items, data }: Props) => (
+const WithStaticProps = ({ items, dataCountries }: Props) => (
   <Layout title="Analytics | Jucy">
     <h1>Youtuber Analytics</h1>
     <List items={items} />
+    <VictoryPie
+      data={dataCountries}
+      animate={{
+        duration: 2000,
+      }}
+    />
     <p>
       <Link href="/">
         <a>Go home</a>
@@ -30,13 +37,24 @@ const WithStaticProps = ({ items, data }: Props) => (
   </Layout>
 )
 
-export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
+export const getServerSideProps: GetStaticProps = async () => {
   const items: User[] = sampleUserData;
   const data: Data = sampleAnalyticData;
-  return { props: { items, data } };
+  const dataCountries: Array<object> = data.items[0].viewPercentWithCountry.map((
+    item: Array<string | number>,
+  ) => { 
+    console.log(item[0]);
+    console.log(Country[item[0]]); // ここら辺の型頑張りたい
+    return {
+      x: Country[item[0]],
+      y: item[1],
+    }
+  });
+  console.log(dataCountries);
+  return { props: {
+    items,
+    dataCountries,
+  } };
 }
 
 export default WithStaticProps
