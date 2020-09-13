e360d988180df3b03312cf39ce8ebf44b2dcfc1f
satooookibi <masa.zin9@gmail.com>
2020-09-13 11:09:04 +0900

    #13 データのグラフ化途中まで

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -1,60 +1,146 @@
-import { GetStaticProps } from 'next';
-import Link from 'next/link';
-import { VictoryPie } from "victory";
-
-import { 
-  Data,
-  User,
- } from '../../types';
- import { Country } from '../../types/analytics';
+import { GetStaticProps } from "next";
+import Link from "next/link";
 import {
-  sampleAnalyticData,
-  sampleUserData
-} from '../../utils/sample-data';
-import Layout from '../../components/Layout';
-import List from '../../components/List';
+  VictoryGroup,
+  VictoryPie,
+  VictoryChart,
+  VictoryBar,
+  VictoryScatter,
+  VictoryTheme,
+  VictoryAxis,
+} from "victory";
+
+import { AgeAndGender } from "../../types";
+import { Data, User } from "../../types";
+
+import { Country, Gender, Age, DoW } from "../../types/analytics";
+import { sampleAnalyticData, sampleUserData } from "../../utils/sample-data";
+import Layout from "../../components/Layout";
+import List from "../../components/List";
 
 type Props = {
-  dataCountries: any,
-  items: User[]
-}
+  dataCountries: any;
+  dataAgeAndGender: any;
+  dataWeekActive: any;
+  items: User[];
+};
 
-const WithStaticProps = ({ items, dataCountries }: Props) => (
+export type dataGender = {
+  male: dataAge[];
+  female: dataAge[];
+};
+
+export type dataAge = {
+  x: string;
+  y: number;
+};
+
+const WithStaticProps = ({
+  items,
+  dataCountries,
+  dataAgeAndGender,
+  dataWeekActive,
+}: Props) => (
   <Layout title="Analytics | Jucy">
     <h1>Youtuber Analytics</h1>
     <List items={items} />
     <VictoryPie
+      theme={VictoryTheme.material}
       data={dataCountries}
       animate={{
         duration: 2000,
       }}
     />
+
+    <VictoryChart theme={VictoryTheme.material} domainPadding={{ x: 8 }}>
+      <VictoryGroup offset={10} colorScale={["#6699FF", "#FF99CC"]}>
+        <VictoryBar data={dataAgeAndGender.male} />
+        <VictoryBar data={dataAgeAndGender.female} />
+      </VictoryGroup>
+    </VictoryChart>
+
+    <div>曜日・時間ごとの視聴率</div>
+    <VictoryChart
+      theme={VictoryTheme.material}
+      domain={{ x: [0, 8], y: [0, 25] }}
+    >
+      <VictoryAxis style={{ ticks: { padding: 5 } }} />
+      <VictoryScatter
+        style={{ data: { fill: "#c43a31" } }}
+        bubbleProperty="amount"
+        maxBubbleSize={5}
+        minBubbleSize={1}
+        data={dataWeekActive}
+      />
+    </VictoryChart>
     <p>
       <Link href="/">
         <a>Go home</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
 export const getServerSideProps: GetStaticProps = async () => {
   const items: User[] = sampleUserData;
   const data: Data = sampleAnalyticData;
-  const dataCountries: Array<object> = data.items[0].viewPercentWithCountry.map((
-    item: Array<string | number>,
-  ) => { 
-    console.log(item[0]);
-    console.log(Country[item[0]]); // ここら辺の型頑張りたい
-    return {
-      x: Country[item[0]],
-      y: item[1],
-    }
-  });
-  console.log(dataCountries);
-  return { props: {
-    items,
-    dataCountries,
-  } };
-}
 
-export default WithStaticProps
+  const dataCountries: Array<object> = data.items[0].viewPercentWithCountry.map(
+    (item: Array<string | number>) => {
+      return {
+        x: Country[item[0]],
+        y: item[1],
+      };
+    }
+  );
+
+  const dataAgeAndGender = data.items[0].ageAndGenderWithViewPercent.reduce(
+    (accumulator: dataGender, currentValue: AgeAndGender) => {
+      if (currentValue[1] == "male") {
+        return {
+          male: accumulator.male.concat({
+            x: currentValue[0].slice(0, 6),
+            y: currentValue[2],
+          }),
+          female: accumulator.female,
+        };
+      } else {
+        return {
+          male: accumulator.male,
+          female: accumulator.female.concat({
+            x: currentValue[0].slice(0, 6),
+            y: currentValue[2],
+          }),
+        };
+      }
+    },
+    { male: [], female: [] }
+  );
+  const dataWeekActive = Object.keys(data.items[0].weekActive).reduce(
+    (accumulator, currentValue) => {
+      return accumulator.concat(
+        Object.keys(data.items[0].hourTimeActive).map((itemHour) => {
+          return {
+            x: DoW[currentValue],
+            y: Number(itemHour),
+            amount:
+              data.items[0].weekActive[currentValue] *
+              data.items[0].hourTimeActive[itemHour],
+          };
+        })
+      );
+    },
+    []
+  );
+  console.log(dataAgeAndGender);
+  return {
+    props: {
+      items,
+      dataCountries,
+      dataAgeAndGender,
+      dataWeekActive,
+    },
+  };
+};
+
+export default WithStaticProps;
