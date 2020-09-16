60a5585796e267b8e2e9841eb26df81ccc46d2f2
sesame-goma <wataru.tatsuda@gmail.com>
2020-09-16 19:20:06 +0900

    d13 Youtube検索画面から分析画面への導線設定

--- src/components/ChannelListRow.tsx
+++ src/components/ChannelListRow.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Link from '../components/link'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -41,45 +42,47 @@ export default function ChannelListRow({item}: Props) {
   const classes = useStyles();
   return (
     <React.Fragment key={item.key}>
-      <Divider component="li" className={classes.divider}/>
-      <ListItem alignItems="flex-start">
-      {/* <ListItem alignItems="flex-start" button onClick={handleClick}> */}
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src={item.thumbnail} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={item.title}
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                {item.description}
-              </Typography>
-            </React.Fragment>
-          }
-        />
-      {/* {open ? <ExpandLess /> : <ExpandMore />} */}
-      </ListItem>
-      {/* <Collapse in={open} timeout="auto" unmountOnExit> */}
-        <List component="div" disablePadding>
-          <ListItem className={classes.nested}>
-            <ListItemIcon>
-              <VisibilityIcon />
-            </ListItemIcon>
-            <ListItemText primary={`総視聴回数: ${parseInt(item.viewCount).toLocaleString()}回`} />
-          </ListItem>
-          <ListItem className={classes.nested}>
-            <ListItemIcon>
-              <SubscriptionsIcon />
-            </ListItemIcon>
-            <ListItemText primary={`チャンネル登録者数: ${parseInt(item.subscriberCount).toLocaleString()}人`} />
-          </ListItem>
-        </List>
-      {/* </Collapse> */}
+      <Link href="/analytics" underline="none" color="inherit">
+        <Divider component="li" className={classes.divider}/>
+        <ListItem alignItems="flex-start">
+        {/* <ListItem alignItems="flex-start" button onClick={handleClick}> */}
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src={item.thumbnail} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={item.title}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  {item.description}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        {/* {open ? <ExpandLess /> : <ExpandMore />} */}
+        </ListItem>
+        {/* <Collapse in={open} timeout="auto" unmountOnExit> */}
+          <List component="div" disablePadding>
+            <ListItem className={classes.nested}>
+              <ListItemIcon>
+                <VisibilityIcon />
+              </ListItemIcon>
+              <ListItemText primary={`総視聴回数: ${parseInt(item.viewCount).toLocaleString()}回`} />
+            </ListItem>
+            <ListItem className={classes.nested}>
+              <ListItemIcon>
+                <SubscriptionsIcon />
+              </ListItemIcon>
+              <ListItemText primary={`チャンネル登録者数: ${parseInt(item.subscriberCount).toLocaleString()}人`} />
+            </ListItem>
+          </List>
+        {/* </Collapse> */}
+      </Link>
     </React.Fragment>
   );
 }
