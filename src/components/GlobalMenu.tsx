31a50b0eb11340c9ccde891f4fed9575b5413991
tomaaaaaaaaa <tomaaaaaaaaa1125@gmail.com>
2020-09-19 19:08:20 +0900

    #34 GMのリファクタリング

--- src/components/GlobalMenu.tsx
+++ src/components/GlobalMenu.tsx
@@ -5,37 +5,9 @@ import Button from '@material-ui/core/Button';
 import Link from '../components/Link';
 import { useUser } from '../utils/firebase/useUser';
 
-
-const userlogined = () => {
+export default function GlobalMenu() {
   const { user, logout } = useUser();
 
-  if (user) {
-    return (
-      <div style={{margin: '0 0 0 auto'}}>
-        <Button variant="contained" color="secondary" onClick={() => logout()}>
-          ログアウト
-        </Button>
-      </div>
-    )
-  } else {
-    return (
-      <div style={{display: 'flex', margin: '0 0 0 auto'}}>
-        <Link style={{paddingRight: '10px'}} href="/eatery/login">
-          <Button variant="contained" color="secondary" disableElevation>
-            飲食店様ログイン
-          </Button>
-        </Link>
-        <Link href="/youtuber/login" color="secondary">
-          <Button variant="contained" color="primary" disableElevation>
-            インフルエンサー様ログイン
-          </Button>
-        </Link>
-      </div>
-    )
-  };
-};
-
-export default function GlobalMenu() {  
   return (
     <div>
       <AppBar position="relative" color="inherit">
@@ -43,7 +15,33 @@ export default function GlobalMenu() {
           <Link href="/" >
             <img src="/logo_sm.png" style={{height: 30, marginTop: 10, marginBottom: 5}}/>
           </Link>
-          {userlogined()}
+
+          {(() => {
+          if (user) {
+            return (
+              <div style={{margin: '0 0 0 auto'}}>
+                <Button variant="contained" color="secondary" onClick={() => logout()}>
+                  ログアウト
+                </Button>
+              </div>
+            )
+          } else {
+            return (
+              <div style={{display: 'flex', margin: '0 0 0 auto'}}>
+                <Link style={{paddingRight: '10px'}} href="/eatery/login">
+                  <Button variant="contained" color="secondary" disableElevation>
+                    飲食店様ログイン
+                  </Button>
+                </Link>
+                <Link href="/youtuber/login" color="secondary">
+                  <Button variant="contained" color="primary" disableElevation>
+                    インフルエンサー様ログイン
+                  </Button>
+                </Link>
+              </div>
+            )
+          };
+          })()}
         </Toolbar>
       </AppBar>
     </div>
