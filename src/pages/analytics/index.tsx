1f4e2adcb6855cacbee2793e792ebbd9d260e128
kterui9019 <pleasure.teru@gmail.com>
2020-09-21 23:03:57 +0900

    ビルドできない問題の修正

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -49,9 +49,58 @@ import Layout from "../../components/Layout";
 import List from "../../components/List";
 import YoutuberInfoPaper from "../../components/YoutuberInfoPaper";
 import YoutubeVideo from "../../components/YoutubeVideo";
-import { useStyles } from "../analytics/styles";
 import useSWR from "swr";
 import { useRegisteredYoutuber } from '../../utils/hooks/useRegisteredYoutuber';
+import { Theme, makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(10),
+    // スクロール禁止のため
+    overflow: 'hidden',
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80vw',
+  },
+  bottomAdjuster: {
+    marginBottom: theme.spacing(5),
+  },
+  gridTile: {
+    marginBottom: theme.spacing(3),
+  },
+  gridBox: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gridTileBar: {
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(25, 118, 210, 0.4)',
+    borderRadius: 5,
+    // backgroundColor: 'rgba(244, 244, 244, 0.8)',
+  },
+  gridCard: {
+    padding: 30,
+  },
+  aws: {
+    marginTop: 30,
+    width: 220,
+    height: 'auto',
+  },
+  firebase: {
+    width: 'auto',
+    height: 190,
+  },
+  skillIcon: {
+    fontSize: theme.spacing(23),
+  },
+  infoIcon: {
+    color: 'rgba(255, 255, 255, 0.65)',
+  },
+}));
 
 const APIKEY = process.env.YOUTUBE_API_KEY;
 
